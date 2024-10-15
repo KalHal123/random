1dff8768fa06e534d846bb26b3b7cfad511e0ffc
KalHal123 <kh7784709@gmail.com>
2024-10-15 20:23:29 +0200

    update commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// Display an alert when the page loads
+window.onload = function() {
+    alert("Welcome to my simple website!");
+};
+
+// Add an event listener to change the intro text when clicked
+document.addEventListener("DOMContentLoaded", function() {
+    const intro = document.getElementById("intro");
+
+    intro.addEventListener("click", function() {
+        intro.querySelector("p").innerText = "You clicked the introduction section!";
+    });
+});
